82a586a2d5e73d00a75ed2baee58cf022739c93c
phyohtetarkar <phyohtetarkar@gmail.com>
2018-07-05 21:44:05 +0630

    update about view for version name

--- app/src/main/java/com/flex/pos/ui/AboutActivity.kt
+++ app/src/main/java/com/flex/pos/ui/AboutActivity.kt
@@ -1,14 +1,18 @@
 package com.flex.pos.ui
 
 import android.os.Bundle
+import com.flex.pos.BuildConfig
 
 import com.flex.pos.R
 import com.flex.pos.ui.views.lock.AutoLockActivity
+import kotlinx.android.synthetic.main.activity_about.*
 
 class AboutActivity : AutoLockActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_about)
+
+        tvAppVersion.text = resources.getString(R.string.app_version, BuildConfig.VERSION_NAME)
     }
 }
